0dc36aead550aa4a32fa91e445870c4cebf730db
Gábor Nácsa <gabor.nacsa@stud.uni-corvinus.hu>
2022-12-07 11:21:57 +0100

    Hajotovabb

--- 9.gyakorlat/wwwroot/hetedik.js
+++ 9.gyakorlat/wwwroot/hetedik.js
@@ -1,29 +1,113 @@
-{
-    fetch('/questions/1')
-        .then(response => response.json())
-        .then(data => kÈrdÈsMegjelenÌtÈs(data));
+Ôªøvar hotList = [];           //Az √©ppen gyakoroltatott k√©rd√©sek list√°ja 
+var questionsInHotList = 3; //Ez majd 7 lesz, tesztel√©shez jobb a 3.
+var displayedQuestion;      //A hotList-b≈ël √©ppen ez a k√©rd√©s van kint
+var numberOfQuestions;      //K√©rd√©sek sz√°ma a teljes adatb√°zisban
+var nextQuestion = 1;       //A k√∂vetkez≈ë k√©rd√©s sz√°ma a teljes list√°ban
+var hotList = [];
+var questionsInHotList = 3; //Ez majd 7 lesz, tesztel√©shez jobb a 3. 
 
-    function kÈrdÈsMegjelenÌtÈs(kÈrdÈs) {
-        console.log(kÈrdÈs);
-        document.getElementById("kÈrdÈs_szˆveg").innerText = kÈrdÈs.questionText
-        document.getElementById("v·lasz1").innerText = kÈrdÈs.answer1
-        document.getElementById("v·lasz2").innerText = kÈrdÈs.answer2
-        document.getElementById("v·lasz3").innerText = kÈrdÈs.answer3
-        document.getElementById("kÈp").src = "https://szoft1.comeback.hu/hajo/" + kÈrdÈs.image;
-    }
 
-    function kÈrdÈsBetˆltÈs(id) {
-        fetch(`/questions/${id}`)
-            .then(v·laszfeldolgoz·s)
-            .then(kÈrdÈsMegjelenÌtÈs);
-    }
-
-    function v·laszfeldolgoz·s(v·lasz) {
-        if (!v·lasz.ok) {
-            console.error(`Hib·s v·lasz: ${response.status}`)
+function Kerdesmegjelenites() {
+        let kerdes = hotList[displayedQuestion].question;
+        console.log(kerdes);
+        document.getElementById("k√©rd√©s_sz√∂veg").innerText = kerdes.question1
+        document.getElementById("v√°lasz1").innerText = kerdes.answer1
+        document.getElementById("v√°lasz2").innerText = kerdes.answer2
+        document.getElementById("v√°lasz3").innerText = kerdes.answer3
+        joValasz = kerdes.correctAnswer;
+        if (kerdes.image) {
+            document.getElementById("k√©p").src = "https://szoft1.comeback.hu/hajo/" + kerdes.image;
+            document.getElementById("k√©p").classList.remove("rejtett")
         }
         else {
-            return v·lasz.json()
+            document.getElementById("k√©p").classList.add("rejtett")
+        }
+        //J√≥ √©s rossz k√©rd√©sek jel√∂l√©s√©nek lev√©tele
+        document.getElementById("v√°lasz1").classList.remove("j√≥", "rossz");
+        document.getElementById("v√°lasz2").classList.remove("j√≥", "rossz");
+        document.getElementById("v√°lasz3").classList.remove("j√≥", "rossz");
+    }
+
+    function kerdesBetoltes(id) {
+        fetch(`/questions/${questionNumber}`)
+            .then(
+                result => {
+                    if (!result.ok) {
+                        console.error(`Hib√°s let√∂lt√©s: ${response.status}`)
+                    }
+                    else {
+                        return result.json()
+                    }
+                }
+            )
+            .then(
+                q => {
+                    hotList[destination].question = q;
+                    hotList[destination].goodAnswers = 0;
+                    console.log(`A ${questionNumber}. k√©rd√©s let√∂ltve a hot list ${destination}. hely√©re`)
+                    if (displayedQuestion == undefined && destination == 0) { //!!!!!!!!!!!!!
+                        displayedQuestion = 0;
+                        Kerdesmegjelenites();
+                    }
+                }
+            );
+    }
+
+    function valaszFeldolgozas(valasz) {
+        if (!valasz.ok) {
+            console.error(`Hib√°s v√°lasz: ${response.status}`)
+        }
+        else {
+            return valasz.json()
         }
     }
+    var joValasz;
+    var questionId = 4
+
+    function elore() {
+        displayedQuestion++;
+        if (displayedQuestion == questionsInHotList) displayedQuestion = 0;
+        Kerdesmegjelenites()
+    }
+
+    function vissza() {
+        questionId--;
+        kerdesBetoltes(questionId)
+    }
+
+    window.onload = function (e) {
+        console.log("Oldal bet√∂ltve...");
+        document.getElementById("el≈ëre_gomb").onclick = elore;
+        document.getElementById("vissza_gomb").onclick = vissza;
+        kerdesBetoltes(questionId)
+        fetch('/questions/1')
+            .then(response => response.json())
+            .then(data => Kerdesmegjelenites(data));
+    }
+
+    function valasztas(n) {
+        if (n != joValasz) {
+            document.getElementById(`v√°lasz${n}`).classList.add("rossz");
+            document.getElementById(`v√°lasz${joValasz}`).classList.add("j√≥");
+        }
+        else {
+            document.getElementById(`v√°lasz${joValasz}`).classList.add("j√≥");
+        }
 }
+
+function init() {
+    for (var i = 0; i < questionsInHotList; i++) {
+        let q = {
+            question: {},
+            goodAnswers: 0
+        }
+        hotList[i] = q;
+    }
+
+    //Els≈ë k√©rd√©sek let√∂lt√©se
+    for (var i = 0; i < questionsInHotList; i++) {
+        k√©rd√©sBet√∂lt√©s(nextQuestion, i);
+        nextQuestion++;
+    }
+}
+
